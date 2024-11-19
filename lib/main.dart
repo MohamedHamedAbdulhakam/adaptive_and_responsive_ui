c2326d217edfa8fc057d883e6b3114274c0a7fdb
MohamedHamedAbdulhakam <mohamedhamedabdulhakam@gmail.com>
2024-11-19 22:22:05 +0200

    layoutMobile part one

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,54 @@ class MediaQuerry extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: LayoutMobile(),
+      ),
+    );
+  }
+}
+
+class LayoutMobile extends StatelessWidget {
+  const LayoutMobile({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
     return ListView.builder(
         itemCount: 10,
         itemBuilder: (context, index) {
-          return ListTile(
-          title:  Text("${index + 1}"),
+          return GestureDetector(
+            onTap: () {
+              Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (context) {
+                return DetailsView(number: index + 1);
+              }));
+            },
+            child: Container(
+              margin: EdgeInsets.all(8),
+              color: Colors.green,
+              child: ListTile(
+                title: Text("${index + 1}"),
+              ),
+            ),
           );
         });
+  }
+}
+
+class DetailsView extends StatelessWidget {
+  const DetailsView({super.key, required this.number});
+  final int number;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Text(
+        number.toString(),
+        style: const TextStyle(fontSize: 35),
+      )),
+    );
   }
 }
