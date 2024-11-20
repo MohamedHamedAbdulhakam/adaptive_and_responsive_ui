0eab29e13d54c0e7af74ce50354dbc980054028e
MohamedHamedAbdulhakam <mohamedhamedabdulhakam@gmail.com>
2024-11-20 13:25:31 +0200

    flexible widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,123 +3,166 @@ import 'dart:developer'; // For logging
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MediaQueryExample());
+  runApp(FlexibleWidget());
 }
 
-class MediaQueryExample extends StatelessWidget {
-  const MediaQueryExample({super.key});
+class FlexibleWidget extends StatelessWidget {
+  const FlexibleWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: LayoutBuilder(
-          builder: (context, constraints) {
-            // Use lowercase 'constraints' for the parameter
-            log(constraints.toString());
-            if (constraints.maxWidth <= 500) {
-              return const LayoutMobile();
-            } // Log the maxWidth correctly
-            else {
-              return DesctopLayout();
-            }
-          },
-        ),
-      ),
-    );
-  }
-}
-
-class LayoutMobile extends StatelessWidget {
-  const LayoutMobile({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: 10,
-      itemBuilder: (context, index) {
-        return GestureDetector(
-          onTap: () {
-            Navigator.of(context).push(MaterialPageRoute(
-              builder: (context) {
-                return DetailsView(number: index + 1);
-              },
-            ));
-          },
-          child: Container(
-            margin: const EdgeInsets.all(8),
-            color: Colors.green,
-            child: ListTile(
-              title: Text("${index + 1}"),
+        body: Column(
+          children: [
+            Flexible(
+              child: Icon(Icons.import_contacts_sharp,
+              size: 200,),
             ),
-          ),
-        );
-      },
-    );
-  }
-}
-
-class DetailsView extends StatelessWidget {
-  const DetailsView({super.key, required this.number});
-  final int number;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(
-          number.toString(),
-          style: const TextStyle(fontSize: 35),
+            Container(
+              height: 300,
+              color: Colors.amber,
+            ),
+            Container(
+              height: 300,
+              color: Colors.red,
+            ),
+            Container(
+              height: 300,
+              color: Colors.green,
+            ),
+          ],
         ),
       ),
     );
   }
 }
 
-class DesctopLayout extends StatefulWidget {
-  const DesctopLayout({super.key});
 
-  @override
-  State<DesctopLayout> createState() => _DesctopLayoutState();
-}
 
-class _DesctopLayoutState extends State<DesctopLayout> {
- int number = 1;
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(
-          child: ListView.builder(
-            itemCount: 10,
-            itemBuilder: (context, index) {
-              return GestureDetector(
-                onTap: () {
-                  number = index + 1;
-                  setState(() {
-                    
-                  });
-                },
-                child: Container(
-                  margin: const EdgeInsets.all(8),
-                  color: Colors.green,
-                  child: ListTile(
-                    title: Text("${index + 1}"),
-                  ),
-                ),
-              );
-            },
-          ),
-        ),
-        Expanded(
-            child: Container(
-          child: Center(
-            child: Text(number.toString()),
-          ),
-        ))
-      ],
-    );
-  }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class MediaQueryExample extends StatelessWidget {
+//   const MediaQueryExample({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         body: 
+//       )
+//     );
+//   }
+// }
+
+// class LayoutMobile extends StatelessWidget {
+//   const LayoutMobile({
+//     super.key,
+//   });
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return ListView.builder(
+//       itemCount: 10,
+//       itemBuilder: (context, index) {
+//         return GestureDetector(
+//           onTap: () {
+//             Navigator.of(context).push(MaterialPageRoute(
+//               builder: (context) {
+//                 return DetailsView(number: index + 1);
+//               },
+//             ));
+//           },
+//           child: Container(
+//             margin: const EdgeInsets.all(8),
+//             color: Colors.green,
+//             child: ListTile(
+//               title: Text("${index + 1}"),
+//             ),
+//           ),
+//         );
+//       },
+//     );
+//   }
+// }
+
+// class DetailsView extends StatelessWidget {
+//   const DetailsView({super.key, required this.number});
+//   final int number;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Center(
+//         child: Text(
+//           number.toString(),
+//           style: const TextStyle(fontSize: 35),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// class DesctopLayout extends StatefulWidget {
+//   const DesctopLayout({super.key});
+
+//   @override
+//   State<DesctopLayout> createState() => _DesctopLayoutState();
+// }
+
+// class _DesctopLayoutState extends State<DesctopLayout> {
+//   int number = 1;
+//   @override
+//   Widget build(BuildContext context) {
+//     return Row(
+//       children: [
+//         Expanded(
+//           child: ListView.builder(
+//             itemCount: 10,
+//             itemBuilder: (context, index) {
+//               return GestureDetector(
+//                 onTap: () {
+//                   number = index + 1;
+//                   setState(() {});
+//                 },
+//                 child: Container(
+//                   margin: const EdgeInsets.all(8),
+//                   color: Colors.green,
+//                   child: ListTile(
+//                     title: Text("${index + 1}"),
+//                   ),
+//                 ),
+//               );
+//             },
+//           ),
+//         ),
+//         Expanded(
+//             child: Container(
+//           child: Center(
+//             child: Text(number.toString()),
+//           ),
+//         ))
+//       ],
+//     );
+//   }
+// }
