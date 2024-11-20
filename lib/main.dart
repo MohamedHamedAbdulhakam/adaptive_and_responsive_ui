368524035c93ef529508db3613f571d31e27d52b
MohamedHamedAbdulhakam <mohamedhamedabdulhakam@gmail.com>
2024-11-20 10:11:22 +0200

    layout builder part two

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,30 @@
+import 'dart:developer'; // For logging
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MediaQuerry());
+  runApp(const MediaQueryExample());
 }
 
-class MediaQuerry extends StatelessWidget {
-  const MediaQuerry({super.key});
+class MediaQueryExample extends StatelessWidget {
+  const MediaQueryExample({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: LayoutMobile(),
+        body: LayoutBuilder(
+          builder: (context, constraints) {
+            // Use lowercase 'constraints' for the parameter
+            log(constraints.toString());
+            if (constraints.maxWidth <= 500) {
+              return const LayoutMobile();
+            } // Log the maxWidth correctly
+            else {
+              return Text('another device');
+            }
+          },
+        ),
       ),
     );
   }
@@ -25,38 +38,42 @@ class LayoutMobile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-        itemCount: 10,
-        itemBuilder: (context, index) {
-          return GestureDetector(
-            onTap: () {
-              Navigator.of(context)
-                  .push(MaterialPageRoute(builder: (context) {
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return GestureDetector(
+          onTap: () {
+            Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) {
                 return DetailsView(number: index + 1);
-              }));
-            },
-            child: Container(
-              margin: EdgeInsets.all(8),
-              color: Colors.green,
-              child: ListTile(
-                title: Text("${index + 1}"),
-              ),
+              },
+            ));
+          },
+          child: Container(
+            margin: const EdgeInsets.all(8),
+            color: Colors.green,
+            child: ListTile(
+              title: Text("${index + 1}"),
             ),
-          );
-        });
+          ),
+        );
+      },
+    );
   }
 }
 
 class DetailsView extends StatelessWidget {
   const DetailsView({super.key, required this.number});
   final int number;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-          child: Text(
-        number.toString(),
-        style: const TextStyle(fontSize: 35),
-      )),
+        child: Text(
+          number.toString(),
+          style: const TextStyle(fontSize: 35),
+        ),
+      ),
     );
   }
 }
