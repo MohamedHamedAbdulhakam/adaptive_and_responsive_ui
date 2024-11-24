ea7fd7fbd660eeb257acf75d18c0efa3629ba1e0
MohamedHamedAbdulhakam <mohamedhamedabdulhakam@gmail.com>
2024-11-24 12:08:20 +0200

    refactor my code .

--- lib/views/widgets/custom_sliver_grid.dart
+++ lib/views/widgets/custom_sliver_grid.dart
@@ -1,3 +1,4 @@
+import 'package:adaptive_and_responsive_ui/views/widgets/sliver_item.dart';
 import 'package:flutter/material.dart';
 
 class CustomSliverGrid extends StatelessWidget {
@@ -6,12 +7,10 @@ class CustomSliverGrid extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SliverGrid.builder(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2,crossAxisSpacing: 16,mainAxisSpacing: 16), itemBuilder: (context,index){
-      return Container(
-        decoration: BoxDecoration(color: (Colors.white),borderRadius: BorderRadius.circular(8)),
-        
-      );
+      return  SliverItem();
       
 
     },itemCount: 4,);
   }
 }
+
---
+++ lib/views/widgets/sliver_item.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SliverItem extends StatelessWidget {
+  const SliverItem({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(color: (Colors.white),borderRadius: BorderRadius.circular(8)),
+      
+    );
+  }
+}
