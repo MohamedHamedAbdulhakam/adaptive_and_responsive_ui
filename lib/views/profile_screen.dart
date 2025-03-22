40bb3698e6e87f69d16adfd51bd11ebbb212001e
MohamedHamed <mohamedhamedabdulhakam@gmail.com>
2025-03-22 23:56:00 +0200

    commit

--- lib/views/profile_screen.dart
+++ lib/views/profile_screen.dart
@@ -5,7 +5,7 @@ class ProfileScreenWithHalfImage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.green,
         elevation: 0,
         leading: IconButton(
           icon: Icon(Icons.arrow_back, color: Colors.white),
